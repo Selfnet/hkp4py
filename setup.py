e5561827bc6e031d5b94f54d6d20ec131d696678
Cellebyte <marcel.fest@live.de>
2018-09-18 21:46:02 +0200

    Updated hkp4py setup.py

--- setup.py
+++ setup.py
@@ -70,7 +70,6 @@ setup(
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    # py_modules=["Multivault"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
