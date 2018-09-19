a1fa5d3394a69394f4bff1c05cac5a946e9c86f5
Cellebyte <marcel.fest@live.de>
2018-09-19 12:46:04 +0200

    Fix setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -52,16 +52,15 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Cryption :: Security',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: MIT',
-
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Security :: Cryptography',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 2 :: 6'
-        'Programming Language :: Python :: 2 :: 7'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
     ],
 
     # What does your project relate to?
